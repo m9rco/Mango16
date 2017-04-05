7d944901f069e9a31bc5506e0aa67c22f8545469
pushaowei <542684913@qq.com>
2017-04-05 10:54:35 +0800

    update login css

--- application/modules/Admin/controllers/Traits/StaticConf.php
+++ application/modules/Admin/controllers/Traits/StaticConf.php
@@ -22,7 +22,7 @@ trait StaticConf
         switch ($page) {
             case 'index.login':
                 $options = [
-                    'title' => '大神降临',
+                    'title' => 'Mango16 - login',
                     'keywords' => '',
                     'description' => '',
                     'style' => [
